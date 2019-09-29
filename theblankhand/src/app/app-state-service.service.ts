12a92b7e29cb8b3863b5597f57b7ab814a1b4feb
Jack Barber <jbarber@solomonic.co.uk>
2019-09-29 23:23:13 +0100

    changed background, font and shadow colour. also started on new view for mobile users

---
+++ theblankhand/src/app/app-state-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppStateServiceService {
+
+  private isMobileRes: boolean;
+
+  constructor() {
+    if (window.innerWidth < 768) {
+      this.isMobileRes = true;
+    } else {
+      this.isMobileRes = false;
+    }
+  }
+
+  public getMobileRes(): boolean {
+    return this.isMobileRes;
+  }
+
+}
--- theblankhand/src/app/homepage/homepage.component.ts
+++ theblankhand/src/app/homepage/homepage.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent implements OnInit {
+export class HomepageComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private elementRef : ElementRef) {
+   }
 
 }
